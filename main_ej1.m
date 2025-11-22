1cd21acfa0d2884ab90d4bed4427f5ce0da6df1c
fransaenz <fransaenzedelp3085@gmail.com>
2025-11-22 16:59:03 -0300

    TFTD aproximada y graficada

--- main_ej1.m
+++ main_ej1.m
@@ -3,13 +3,31 @@
 %carga la senial con un numero de alumno como parametro
 [x, n] = senial(038973);
 
-%grafica
+%grafica de la funcion
 figure(1);
 stem(n, x);
 grid on;
 title("Señal x[n]");
 xlabel("n"); ylabel("x[n]");
 
+%obtencion de TFTD aproximada mediante la TDF con N potencia de 2
+x = x(:)';
+L = length(x);
+M = 2^nextpow2(L); %relleno de ceros
+X = fft(x, M);     %calculo de TDF
 
+%grafico del modulo de X
+modulo_X = abs(X);
+s = (0:N-1)/N;
+figure;
+plot(s, modulo_X); grid on;
+xlabel('s');
+ylabel('|X(s)|');
 
+%grafico de la fase de X
+phaseX_unwrapped = unwrap(phaseX);
 
+figure;
+plot(s, phaseX_unwrapped); grid on;
+xlabel('s');
+ylabel('arg(X(s))[rad]');
