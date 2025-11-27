12c1b0b943add9ea3412fad7a72b10de58d40950
fransaenz <fransaenzedelp3085@gmail.com>
2025-11-27 01:49:41 -0300

    ejercicio 1.2 completo

--- sistema2.m
+++ sistema2.m
@@ -1,4 +1,4 @@
-%%respuesta impulsional del sistema 1
+%%respuesta impulsional del sistema 2
 
  %defino cuantas muestras de h quiero en la grafica
 N = 5;
@@ -26,6 +26,29 @@ stem(L , h, 'filled', 'MarkerSize', 3);
 grid on;
 xlabel('n');
 ylabel('h[n]');
-title('Respuesta impulsional h[n] del Sistema 1');
+title('Respuesta impulsional h[n] del Sistema 2');
 
-%%respuesta en frecuencia del sistema 1
+%%respuesta en frecuencia del sistema 2
+M = 1000;
+s = linspace(-1, 1, M);
+H = 1/2 - (1/2)*exp(-j*2*pi*s);
+
+%grafico del modulo
+modulo_H = abs(H);
+figure;
+plot(s, modulo_H);
+grid on;
+xlabel('s');
+ylabel('|H(e^{j2πs})|','interpreter','latex');
+title('Modulo de la respuesta frecuencial H(e^{j2πs}) del Sistema 2', 'interpreter', 'latex');
+ylim([min(modulo_H), max(modulo_H)+0.1]);
+
+%grafico del fase
+figure;
+fase_H = angle(H);
+plot(s, fase_H);
+grid on;
+xlabel('s');
+ylabel('arg(e^{j2πs})','interpreter','latex');
+title('Fase de la respuesta frecuencial  H(e^{j2πs}) del Sistema 2', 'interpreter', 'latex');;
+
--- sistema4.m
+++ sistema4.m
@@ -30,3 +30,26 @@ title('Respuesta impulsional h[n] del Sistema 4');
 
 %%respuesta en frecuencia del sistema 4
 
+M = 1000;
+s = linspace(-1, 1, M);
+H = (1/4) * (1 - exp(-j*2*pi*s)) ./ (1 + 0.5*exp(-j*2*pi*s));
+
+%grafico del modulo
+modulo_H = abs(H)
+figure;
+plot(s, modulo_H);
+grid on;
+xlabel('s');
+ylabel('|H(e^{j2πs})|','interpreter','latex');
+title('Modulo de la respuesta frecuencial H(e^{j2πs}) del Sistema 4', 'interpreter', 'latex');
+ylim([min(modulo_H), max(modulo_H)+0.1]);
+
+%grafico del fase
+figure;
+fase_H = angle(H)
+plot(s, fase_H);
+grid on;
+xlabel('s');
+ylabel('arg(e^{j2πs})','interpreter','latex');
+title('Fase de la respuesta frecuencial  H(e^{j2πs}) del Sistema 4', 'interpreter', 'latex');;
+
